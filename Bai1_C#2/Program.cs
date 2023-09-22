9d65a1ca6a5bc70693314b3cafbf80505dfb00c4
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2023-09-23 01:30:09 +0700

    Add project files.

---
+++ Bai1_C#2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_C_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            List<Bike> lst = new List<Bike>() 
+            {
+                new Bike(1,"xe dap 1","vn"),
+                new Bike(2,"xe dap 2","tau`"),
+                new Bike(3,"xe dap 3","trung quoc"),
+                new Bike(4,"xe dap 4","dong lao`"),
+                new Bike(5,"xe dap 5","dai loan"),
+                new Bike(6,"xe dap 6","vn"),
+                new Bike(7,"xe dap 7","tau`"),
+            };
+            string path = @"D:\FPT\Factory\ProgressTests\Bai1_C#2\Bai1_C#2\BikeFile.txt";
+            SERVICE sv = new SERVICE();
+            int chon;
+            do
+            {
+                Console.WriteLine("_________MENU_________");
+                Console.WriteLine("1. Nhập");
+                Console.WriteLine("2. Xuất");
+                Console.WriteLine("3. Ghi file");
+                Console.WriteLine("4. Đọc file");
+                Console.WriteLine("5. Xoa theo ID");
+                Console.WriteLine("0. Thoat");
+                Console.WriteLine("Moi chon chuong trinh");
+                chon = Convert.ToInt32(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1: sv.Nhap(lst);
+                        break;
+                    case 2: sv.Xuat(lst);
+                        break;
+                    case 3: File.WriteAllText(path, ""); 
+                        sv.GhiFile(path, lst);
+                        break;
+                    case 4: List<Bike> bikes = sv.DocFile(path);
+                        foreach (var item in bikes)
+                        {
+                            item.InThongTin();
+                        }
+                        break;
+                    case 5: sv.XoaBike(lst);
+                        break;
+                    case 0:
+                        Console.WriteLine("Nhan phim bat ky de thoat");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            } while (chon!=0);
+        }
+    }
+}
---
+++ Bai1_C#2/SERVICE.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_C_2
+{
+    internal class SERVICE
+    {
+        public void Nhap(List<Bike> lst)
+        {
+            string tiepTuc;
+            int id = 1;
+            do
+            {
+                Bike bk = new Bike();
+                //Console.WriteLine("Moi nhap id");
+                foreach (var item in lst)
+                {
+                    if (item.Id == id)
+                    {
+                        id++;
+                    }
+                }
+                bk.Id = id;
+                Console.WriteLine("Moi nhap ten");
+                bk.Ten = Console.ReadLine();
+                Console.WriteLine("Moi nhap hang san xuat");
+                bk.Hsx = Console.ReadLine();
+                lst.Add(bk);
+                Console.WriteLine("Ban co muon nhap tiep? (co/ko)");
+                tiepTuc = Console.ReadLine();
+            } while (tiepTuc.Equals("co"));
+        }
+
+        public void Xuat(List<Bike> lst)
+        {
+            if (lst.Count == 0)
+            {
+                Console.WriteLine("DS trong");
+                return;
+            }
+            foreach (var item in lst)
+            {
+                item.InThongTin();
+            }
+        }
+        public void GhiFile(string path, List<Bike> lst)
+        {
+            if (File.Exists(path))
+            {
+                foreach (var item in lst)
+                {
+                    string line = item.ObjToString();
+                    File.AppendAllText(path, line);
+                }
+                Console.WriteLine("Ghi File thanh cong");
+            }
+            else
+            {
+                Console.WriteLine("File khong ton tai");
+            }
+        }
+
+        public List<Bike> DocFile(string path)
+        {
+            List<Bike> lst = new List<Bike>();
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                if (line.Trim().Length == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    string[] attribute = line.Split('|');
+                    Bike bk = new Bike();
+                    bk.Id = Convert.ToInt32(attribute[0]);
+                    bk.Ten = attribute[1];
+                    bk.Hsx = attribute[2];
+                    lst.Add(bk);
+                }
+            }
+            return lst;
+        }
+
+        public void XoaBike(List<Bike> lst)
+        {
+            Console.WriteLine("Moi nhap id xe muon xoa");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Bike obj = lst.FirstOrDefault(x=>x.Id == id);
+            if (obj == null)
+            {
+                Console.WriteLine("Khong tim duoc xe nao");
+            }
+            else
+            {
+                foreach (var item in lst)
+                {
+                    if (item.Id == obj.Id)
+                    {
+                        lst.Remove(item);
+                    }
+                }
+            }
+        }
+    }
+}
